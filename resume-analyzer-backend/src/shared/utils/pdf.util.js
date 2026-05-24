8e67c3fbaf680ddb6a4a2fa0234289115a7e40ed
Loveneesh <l.ksingla2@gmail.com>
2026-05-24 22:10:49 +0530

    removed unnecessary log

--- resume-analyzer-backend/src/shared/utils/pdf.util.js
+++ resume-analyzer-backend/src/shared/utils/pdf.util.js
@@ -6,6 +6,5 @@ const pdfParse = require("pdf-parse"); // ✅ now works
 export async function extractTextFromPDF(buffer) {
   const data = await pdfParse(buffer);
 
-  console.log("Extracted text: =>>>>>", data.text);
   return data.text;
 }
